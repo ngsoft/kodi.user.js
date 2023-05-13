dc96eb271ef640a3eeab011c69fb49282a920acf
Aymeric Anger <daedelus.git@gmail.com>
2023-05-13 15:40:08 +0200

    parse

--- src/reaperdl.user.js
+++ src/reaperdl.user.js
@@ -14,12 +14,12 @@
     const fetch = root['tamper-fetch'].fetch_timeout;
 
 
-    let isBeta = /^beta/.test(location.host), currentChapter = null, downloading = false, dlgif = false, series;
+    let isBeta = /^beta/.test(location.host), currentChapter = null, downloading = false, dlgif = false, series, isChapter = false;
 
 
     function getImageList(doc){
         let images = [];
-        doc.querySelectorAll('.main-container img[src*="img.asurascans.com"], #readerarea img[decoding], .entry-content .separator img[src*="cdn.bakihanma.com"]')
+        doc.querySelectorAll('img.max-w-full')
                 .forEach(img => images.push(img.src));
         return images.filter(src => (! src.endsWith('.gif') || dlgif));
     }
@@ -46,202 +46,41 @@
 
     }
 
-    if (/asura/.test(location.host))
-    {
 
 
 
 
-        async function searchBeta(query){
-
-            if (! query)
-            {
-                throw new Error('invalid query');
-            }
-
-            let src = new URL('https://www.asurascans.com/');
-
-            src.searchParams.set('s', query);
-
-            let doc = await fetch(src)
-                    .then(handleFetchError)
-                    .then(resp => resp.text())
-                    .then(txt => html2doc(txt));
-
-            let results = [];
-
-
-            doc.querySelectorAll('.listupd a[title]').forEach(a => {
-                results.push({
-                    title: a.title,
-                    href: a.href,
-                    cover: a.querySelector('img').src
-                });
-            });
-
-
-            return results;
-        }
 
 
 
-        series = (() => {
-            if (isBeta)
-            {
+    history.pushState = (function(){
 
 
-                let
-                        details = document.querySelector('#__next main .series-details'),
-                        isChapter = document.querySelector('#__next main .chapter-section .main-container .container');
+        const {pushState} = history;
 
-                let
-                        chapterList = [],
-                        title = details !== null ?
-                        details.querySelector('h2').innerText :
-                        isChapter.querySelector('a[href*="/comics/"]').innerText,
-                        ulist = [];
+        return function(state, title, url){
 
-
-                if (details !== null)
-                {
-
-
-                    document.querySelectorAll('.chapters-section a[href*="/read/"]').forEach(el => {
-
-                        if (! ulist.includes(el.href))
-                        {
-                            chapterList.push(new Chapter(el.href, el.querySelector('h5.chapter-link').innerText, getImageList));
-                            ulist.push(el.href);
-                        }
-
-                    });
-                } else
-                {
-                    document.querySelector('.chapter-section .dropdown-menu').querySelectorAll('a.dropdown-item').forEach(el => {
-
-                        let href = el.href, current = el.classList.contains('active'), chap;
-                        if (href === '')
-                        {
-                            href = location.href;
-                        }
-                        if (! ulist.includes(href))
-                        {
-                            chapterList.push((chap = new Chapter(href, el.innerText, getImageList)));
-                            ulist.push(href);
-                        }
-
-                        if (current)
-                        {
-                            currentChapter = chap;
-                        }
-
-
-
-                    });
-
-
+            dispatchEvent(Object.assign(new Event('page.pushstate'), {
+                data: {
+                    state,
+                    title,
+                    url
                 }
-
-                return new Manga(title, chapterList, true, searchBeta);
-            }
-
-            let
-                    details = document.querySelector('.infox'),
-                    isChapter = document.querySelector('.chapterbody .postarea article');
-
-            let
-                    chapterList = [],
-                    title = details !== null ?
-                    details.querySelector('h1.entry-title').innerText :
-                    isChapter.querySelector('.headpost .allc a').innerText,
-                    ulist = [];
+            }));
+            return pushState.call(history, state, title, url);
+        };
 
 
-            if (details !== null)
-            {
-                document.querySelectorAll('#chapterlist .chbox a').forEach(el => {
-                    if (! ulist.includes(el.href))
-                    {
-                        chapterList.push(new Chapter(el.href, el.querySelector('.chapternum').innerText, getImageList));
-                        ulist.push(el.href);
-                    }
-
-                });
-            } else
-            {
-                document.querySelector('.selector').querySelectorAll('select#chapter option[value*="https"]').forEach(el => {
-
-                    let href = el.value, current = el.selected === true, chap;
-                    if (! ulist.includes(href))
-                    {
-                        chapterList.push((chap = new Chapter(href, el.innerText, getImageList)));
-                        ulist.push(href);
-                    }
-
-                    if (current)
-                    {
-                        currentChapter = chap;
-                    }
-
-                });
-
-
-            }
-
-
-
-            return new Manga(title, chapterList.reverse());
-
-        })();
-
-
-
-
-    } else if (/soldier/.test(location.host))
-    {
-
-        const DIFF_ID = 987;
-
-        let title = 'The Skeleton Soldier Failed to Defend the Dungeon', chapterList, _chapterList = document.querySelectorAll('.entry-content li.su-post a');
-
-
-        if (_chapterList.length > 0)
-        {
-
-            chapterList = Array.from(_chapterList).map(el => {
-                let id = /(\d+)\/$/.exec(el.href)[1];
-
-                for (let i = id.length; i < 3; i ++)
-                {
-                    id = '0' + id;
-                }
-
-
-                return new Chapter(el.href, 'Chapter ' + id, getImageList);
-
-
-            }).reverse();
-
-            series = new Manga(title, chapterList);
-
-
-        }
+    })();
 
 
 
 
 
-    }
 
 
+    function downloadChapter(selection, ui, qlength = 2, chaptersPerZip = 20){
 
-    function downloadChapter(selection, ui, qlength = 5, chaptersPerZip = 20){
-
-        if (/soldier/.test(location.host))
-        {
-            qlength = 1;
-            chaptersPerZip = 10;
-        }
 
         return new Promise((resolve, reject) => {
 
@@ -251,7 +90,8 @@
                     root = mainprogressbar.elements.root,
                     current = 0,
                     success = [],
-                    failed = [];
+                    failed = [],
+                    series = getSeries();
 
 
             mainprogressbar.label = 'Download Queue';
@@ -356,7 +196,7 @@
                             controller.abort();
                         });
 
-                        return chapter.getPDF(progress, signal).then(pdf => {
+                        return chapter.getPDF(progress, signal, 20, 3).then(pdf => {
                             if (folder)
                             {
                                 folder.file(chapter.label + '.pdf', pdf, {base64: true});
@@ -435,34 +275,77 @@
 
 
 
+    function getSeries(){
 
-    menu.clear();
 
-    if (series)
-    {
+        if (! series)
+        {
+            let title = document.querySelector('.container h1, main .text-2xl').innerText.trim(), chapters = [];
+            console.debug(title);
+            if (! isChapter)
+            {
+                document.querySelectorAll('ul a[href*="/chapters/"]').forEach(el => {
+                    let label = el.querySelector('p.truncate').innerText, link = el.href;
+                    chapters.push(new Chapter(link, label, getImageList));
+                });
 
-        if (currentChapter !== null)
+            } else
+            {
+                chapters.push(currentChapter = new Chapter(location.href, document.querySelector('main nav .hidden').innerText, getImageList))
+            }
+
+
+            series = new Manga(title, chapters.reverse());
+
+            console.debug(series, chapters, currentChapter, isChapter);
+        }
+
+
+
+        return series;
+    }
+
+
+
+
+
+    function main(){
+
+        isChapter = /\/chapters\//.test(location.pathname);
+        // mainPage = (document.querySelector('a .fa-list')?.parentElement.href) ?? location.href.split('?')[0];
+
+        currentChapter = series = null;
+
+
+
+        menu.clear();
+
+
+        if (isChapter)
         {
             menu.addItem('Download current chapter', () => {
                 if (! downloading)
                 {
-                    Overlay.downloadSelection(series, currentChapter).then(ui => {
+                    Overlay.downloadSelection(getSeries(), currentChapter).then(ui => {
                         downloadChapter([currentChapter], ui);
                     });
                     menu.removeItem('chapdl');
                 }
 
             }, 'chapdl');
+
+
+
         }
 
 
-        Overlay.getInstance(series).hide().then(ui => {
+        Overlay.getInstance(getSeries()).hide().then(ui => {
             ui.on('chapter.selected', e => {
 
                 if (! downloading)
                 {
                     let sel = e.detail;
-                    Overlay.downloadSelection(series, sel).then(ui => {
+                    Overlay.downloadSelection(getSeries(), sel).then(ui => {
                         downloadChapter(sel, ui);
                     });
                 }
@@ -477,15 +360,18 @@
 
             if (! downloading)
             {
-                Overlay.getSelection(series);
+                Overlay.getSelection(getSeries());
             }
 
 
         }, 'mangadl');
+
     }
 
 
+    addEventListener('page.pushstate', main);
 
+    main();
 
 
 })(typeof unsafeWindow !== 'undefined' ? unsafeWindow : window, typeof self !== 'undefined' ? self : this);
